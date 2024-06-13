6647ba3a931fa6df734b051fb3075983ce4944ab
NY64 <me@ny64.kr>
2024-06-13 18:29:26 +0900

    refactor: Rename function localhost_only to authorize_request

--- app/routers/slunch_noti.py
+++ app/routers/slunch_noti.py
@@ -24,49 +24,20 @@ def get_db():
     finally:
         db.close()
 
-def localhost_only(request: Request):
+def authorize_request(request: Request):
     secret_key = os.environ["SECRET_KEY"]
     header_key = request.headers.get("X-Secret-Key")
     if header_key != secret_key:
         raise HTTPException(status_code=403, detail="Not authorized")
 
-@router.get("/subscribe", responses={
-    200: {"model": Response, "description": "구독 성공"},
-    400: {"model": ErrorResponse, "description": "구독 실패"}
-})
-def subscribe(
-    token: str = Query(..., title="토큰", description="FCM 토큰"),
-) -> Response | ErrorResponse:
-    with get_db() as db:
-        try:
-            db.execute("INSERT INTO slunch_noti (token) VALUES (?)", (token,))
-            return Response(data="구독 성공")
-        except Exception as e:
-            return ErrorResponse(error=str(e))
-
-@router.get("/unsubscribe", responses={
-    200: {"model": Response, "description": "구독 해지 성공"},
-    400: {"model": ErrorResponse, "description": "구독 해지 실패"}
-})
-def unsubscribe(
-    token: str = Query(..., title="토큰", description="FCM 토큰"),
-) -> Response | ErrorResponse:
-   with get_db() as db:
-        try:
-            db.execute("DELETE FROM slunch_noti WHERE token=?", (token,))
-            return Response(data="구독 해지 성공")
-        except Exception as e:
-            return ErrorResponse(error=str(e))
-        
-@router.post("/send", dependencies=[Depends(localhost_only)], responses={
+@router.post("/send", dependencies=[Depends(authorize_request)], responses={
     200: {"model": Response, "description": "푸시 알림 전송 성공"},
     400: {"model": ErrorResponse, "description": "푸시 알림 전송 실패"},
-    403: {"model": ErrorResponse, "description": "허용되지 않은 IP 주소"}
+    403: {"model": ErrorResponse, "description": "접근 권한 없음"}
 })
 def send(
     notification: PushNotification = Body(...),
 ) -> Response | ErrorResponse:
-    # with get_db() as db:
     try:            
         success, faild = 0, 0
         
