55873eb206f604aebef0065e9e952412a4be3a5a
NY64 <me@ny64.kr>
2024-06-28 11:59:48 +0900

    refactor: Update find and find_one methods in database.py to support optional projection

--- app/libs/database.py
+++ app/libs/database.py
@@ -8,18 +8,17 @@ class db:
     def insert(self, collection_name, data):
         self.db[collection_name].insert_one(data)
 
-    def find(self, collection_name, query):
-        return self.db[collection_name].find(query)
+    def find(self, collection_name, query, projection=None):
+        return self.db[collection_name].find(query, projection)
 
-    def find_one(self, collection_name, query):
-        return self.db[collection_name].find_one(query)
+    def find_one(self, collection_name, query, projection=None):
+        return self.db[collection_name].find_one(query, projection)
     
     def update(self, collection_name, query, data):
         self.db[collection_name].update_one(query, {'$set': data})
 
     def delete(self, collection_name, query):
         self.db[collection_name].delete_one(query)
-        
         
     def close(self):
         self.client.close()
